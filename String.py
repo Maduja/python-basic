4806a5d9ab8faccdb77e4d2e5dda31bbab6fa297
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2025-01-13 11:47:13 +0530

    01.13

---
+++ String.py
@@ -0,0 +1,38 @@
+my_string='hello'
+print(my_string)
+
+my_string="hello"
+print(my_string)
+
+my_string='''Hello'''
+print(my_string)
+
+my_string=  """hello,
+            my name is 
+            maduja"""
+print(my_string)
+print('------------------------------------------------')
+
+
+str='jvlcode'
+print(str)
+
+print(str[0])
+print(str[-1])
+
+print(str[0:3])
+print(str[3:-1])
+
+print('-----------------------------')
+
+
+str1="Hello"
+str2=" world"
+
+print(str1 + str2)
+
+print(str1*3)
+
+
+
+
