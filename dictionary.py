3edf48f36ac635324078a5349fc1778b02c5a8de
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2025-01-13 22:06:50 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,16 @@ my_dict={}
 my_dict={1:"maduja",2:"iran"}
 print(my_dict)
 
+person={"name":"nimal",'age':25}
+print(person)
+
+person['age']=40
+print(person)
+
+person['salary']=10000
+print(person)
+
+del person ['age']
+print(person)
+
+del person
