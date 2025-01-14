36952217a6ecd1c5fcb665e95a4f1ca904d39d21
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2025-01-14 17:25:09 +0530

    Update range.py

--- range.py
+++ range.py
@@ -3,3 +3,13 @@ print(range(1,10))
 numbers=range(1,6)
 print(list(numbers))
 print(tuple(numbers))
+
+numbers1=range(1,6,1)
+print(list(numbers1))
+
+numbers1=range(1,6,2)
+print(list(numbers1))
+
+numbers1=range(5,0,-1)
+print(list(numbers1))
+
